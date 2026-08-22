4eb6dc31f9e690d2947ce2ddcd181e379492b4f3
Sergey Natalenko <sergey.natalenko@mail.ru>
2026-08-22 15:57:17 +0300

    style: avoid assert in production auth code

--- asyncly/client/base.py
+++ asyncly/client/base.py
@@ -300,7 +300,8 @@ def _normalize_auth_kwargs(
         kwargs["proxy_headers"] = proxy_headers
         _add_proxy_auth_middleware(kwargs)
         return
-    assert proxy_auth is not None
+    if proxy_auth is None:
+        return
     encoded = _encode_basic_auth(
         proxy_auth.login, proxy_auth.password, proxy_auth.encoding
     )
